c263a3e8637070b9312b59296a85723fcf8d9054
akram56789 <akramhossain56789@gmail>
2022-03-15 21:31:19 +0600

    commit

--- js/countries.js
+++ js/countries.js
@@ -4,7 +4,7 @@ const loadCountries = () => {
         .then(data => displayCountries(data));
 }
 const displayCountries = countries => {
-    // console.log(countries[0]);
+    console.log(countries[0]);
     const countriesHTML = countries.map(country => getCountryHTML(country))
     // console.log(countriesHTML[0])
     const container = document.getElementById('countries')
@@ -12,14 +12,50 @@ const displayCountries = countries => {
 
 
 }
-const getCountryHTML = country => {
+
+// option 2
+const getCountryHTML = ({ name, flags, region, population, timezones }) => {
     return `
       <div class="country">
-       <h2>${country.name.common}</h2>
-       <img src = "${country.flags.png}">
-    
+       <h2>${name.common}</h2>
+       <p>rigion:${region}</p>
+       <p>independent:${population}</p>
+       <p>time:${timezones[0]}</p>
+       
+     
+       <img src = "${flags.png}">
+
       </div>
     `
 }
+
+
+
+
+//   original 
+// const getCountryHTML = country => {
+//     // ekhane distructur kore hoice 
+//     const { name, flags } = country;
+//     return `
+//       <div class="country">
+//        <h2>${country.name.common}</h2>
+//        <img src = "${country.flags.png}">
+
+//       </div>
+//     `
+// }
+
+// option 1
+// const getCountryHTML = country => {
+//     // ekhane distructur kore hoice 
+//     const { name, flags } = country;
+//     return `
+//       <div class="country">
+//        <h2>${name.common}</h2>
+//        <img src = "${flags.png}">
+
+//       </div>
+//     `
+// }
 
 loadCountries()
